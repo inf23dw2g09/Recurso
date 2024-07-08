16cd8ca59569dc644b40acc84f4ccbbe41728f9b
N0rtenh0 <100228466+N0rtenh0@users.noreply.github.com>
2024-07-08 01:49:36 +0100

    Exame Recurso - Falta documentação e Readme

---
+++ M1M2/M2/api/src/utils/db.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'Pokemon_db_M2', // Make sure this is the correct host name
+  user: 'Pokemon',
+  password: 'Pokemon',
+  database: 'pokemon',
+  port: 3306
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log('Error on database connection.');
+    throw err;
+  }
+  console.log('Database connection active.');
+});
+
+module.exports = connection;
